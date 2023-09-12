8499b64868f8fa9260d31937d7ceb4609e7dc9a5
Christina <cph-ck318@cphbusiness.dk>
2023-09-12 11:09:25 +0200

    kommentarer

--- create-post.js
+++ create-post.js
@@ -1,8 +1,10 @@
 export function createPost(x){
+    // Hiver fatt i skabelonen fra HTML template
     const template = document.getElementById("posts");
 
+    // Cloner template
     const clone = document.importNode(template.content, true);
 
+    // Sætter den udfyldte kopi ind på hjemmesiden
     template.appendChild(clone);
-    
 }
