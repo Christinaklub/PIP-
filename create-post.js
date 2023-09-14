dc39a022ddaaffab88ee22d69e5ebb3176beae45
Christina <cph-ck318@cphbusiness.dk>
2023-09-14 11:23:00 +0200

    template

--- create-post.js
+++ create-post.js
@@ -1,13 +1,24 @@
 export function createPost(x){
-    // Hiver fatt i skabelonen fra HTML template
-    const template = document.getElementById("posts");
 
-    // Cloner template
-    const clone = document.importNode(template.content, true);
+    const allPosts = ["posts"]
 
+    allPosts.forEach((x) => {
+        console.log(x.id);
+    
+        // Hiver fatt i skabelonen fra HTML template
+        const template = document.getElementById("posts");
 
-    // fyll ind template - alle elementer som ligger i template?  
+        // Cloner template
+        const clone = document.importNode(template.content, true);
 
+    
+        //cloner nye elementer i template
+        const postImg = clone.getElementByClass("post-pic");
+        postImg.id = "post-pic" + x.id; 
+    
+        const postText = clone.getElementByClass("post-text");
+        postText.id = "post-text" + x.id; 
+        })
 
     // Sætter den udfyldte kopi ind på hjemmesiden
     template.appendChild(clone);
